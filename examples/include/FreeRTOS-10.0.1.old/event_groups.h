4627ad740899319be0c106140fec3dbd5b0fcc22
Alexander Senier <senier@componolit.com>
2021-01-31 23:18:35 +0100

    Delete old files

--- examples/include/FreeRTOS-10.0.1.old/event_groups.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-typedef uint64_t EventBits_t;
-typedef uint64_t EventGroupHandle_t;
-
-#define xEventGroupSetBits(x, y)
--- examples/include/FreeRTOS-10.0.1.old/list.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-typedef uint64_t ListItem_t;
-typedef uint64_t List_t;
-
-#define vListInsertEnd(x, y)
